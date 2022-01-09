27f6dcb19b3048c1050c6867a00971fbb9d644ae
Gyandeep <gyandeepmehra370@gmail.com>
2022-01-09 08:09:36 +0530

    Add student 4

---
+++ OOPS/student4.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class student4 {
+    public static class Student {
+        int age;
+        String name;
+
+        void announceYourself() {
+            System.out.println(this.name + "[" + this.age + "] is present.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Student s1; // s1 stores null
+        s1 = new Student(); // new will allocate memory in heap, and create objen age and name
+        s1.age = 34;
+        s1.name = "A";
+
+        Student s2 = new Student();
+        s2.age = 10;
+        s2.name = "B";
+
+        s1.announceYourself();
+        s2.announceYourself();
+
+        swap(s1, s2);
+
+        s1.announceYourself();
+        s2.announceYourself();
+
+    }
+
+    public static void swap(Student s1, Student s2) {
+        s2 = new Student();
+
+        int tage = s1.age;
+        s1.age = s2.age;
+        s2.age = tage;
+
+        String tname = s1.name;
+        s1.name = s2.name;
+        s2.name = tname;
+    }
+}
